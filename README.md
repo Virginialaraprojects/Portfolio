c8c66a4030f943b95427071099eb2c3c148ed921
Virginialaraprojects <106257931+Virginialaraprojects@users.noreply.github.com>
2022-07-10 17:05:54 -0700

    css styling

---
+++ README.md
@@ -0,0 +1,3 @@
+yellow= FBE8A6
+ navy=303C6C
+ orange f4976c
--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 const express= require('express');
-const { nextTick } = require('process');
-
 const app =express();
-
 const { data }=require('./data.json');
 
+/****** View engine ******/
 app.set('view engine','pug')
 
 app.use('/static',express.static('public'));
 
+/****** GET home page ******/
 app.get('/', (req,res)=>{
+//pass all the project data to the index template
     res.render('index',{data});
 });
 
+/******  GET About page ******/
 app.get('/about',(req,res)=>{
     res.render('about');
 });
 
-app.get('/project/:id',(req,res)=>{
-    const { id }=req.params.id;
-    const data= data[id];
-    if(data){
-        res.render('project',{ data })
+/****** GET Project page ******/
+app.get('/project/:id',function(req,res,next){
+    const  projectId =req.params.id;
+    const  project = data.find(({id})=> id === +projectId);
+    if(project){
+    //pass the project data to project template
+        res.render('project',{data});
     }else{
-    next();
+        res.sendStatus(404);
     }
 })  
 /****** 404 Error Handler ******/
@@ -37,6 +40,8 @@ app.use((req,res,next)=>{
 /****** Global Error Handler ******/
 
 
+
+// display app on screen
 app.listen(3000,()=>{
     console.log('The application is running on the localhost:3000!')
 });
